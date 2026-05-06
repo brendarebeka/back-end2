a35f2c899de9ba8dac77b69057cfd95f91d3bf6b
blow me up <brenda.marini@escola.pr.gov.br>
2026-05-06 12:29:46 +0000

    versao2

--- cardapio.js
+++ cardapio.js
@@ -5,13 +5,46 @@ const cardapio = [
     {nome: "Milkshake", preco: 12.00, categoria: "sobremesa"}
 ];
 
-
 const pedido = [];
 
 console.log(cardapio[0].nome);
 console.log(cardapio[1].preco);
 console.log(cardapio[2].categoria);
 
+const cliente {
+    nome: "Ana",
+    fidelidade: true
+
+};
+
+function calcularComDesconto(total, clienteObj) {
+    if (clienteObj.fidelidade === true) {
+        let desconto = total * 0.10;
+        console.log(`Desconto fidelidade: R$ ${desconto}`)
+        return total - desconto;
+    }
+    return total;
+}
+
 function exibirCardapio(lista) {
     console.log("===CARDÁPIO===")
-    console.log('')
+    console.log(`1. ${lista[0].nome} R$ ${lista[0].preco}`);
+    console.log(`2. ${lista[1].nome} R$ ${lista[1].preco}`);
+    console.log(`3. ${lista[2].nome} R$ ${lista [2].preco}`);
+    console.log(`4. ${lista [3].nome} R$ ${lista[3].preco}`);
+}
+
+function calcularTotal (listaPedido) {
+    let total = listaPedido[0].preco + listaPedido[1].preco;
+    return total;
+}
+
+function exibirPedido(listaPedido) {
+    console.log("=== SEU PEDIDO ===");
+    console.log(`1. ${listaPedido[0].nome} - R$ ${listaPedido[0].preco}`);
+    console.log(`2. ${listaPedido[1].nome} - R$ ${listaPedido[0].preco}`);
+    console.log(`TOTAL: R$ ${calcularTotal(listaPedido)}`);
+}
+
+ exibirCardapio(cardapio);
+ exibirPedido(pedido);
