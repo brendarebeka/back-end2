155cdc3dec6dac99e1573a6e9f2f2537ebc4aac1
blow me up <brenda.marini@escola.pr.gov.br>
2026-06-22 12:10:04 +0000

    versao5

---
+++ escola.js
@@ -0,0 +1,13 @@
+const dadosDoAluno = require('./aluno.json');
+
+console.log("--- Sistema escolar ---");
+console.log(`Nome: ${dadosDoAluno.nome}`);
+console.log(`Curso: ${dadosDoAluno.curso}`);
+
+if (dadosDoAluno.bolsista) {
+    console.log("Status: Este aluno possui isenção de mensalidade.");
+} else {
+console.log("Status: Mensalidade padrão.");
+};
+
+console.log(`${dadosDoAluno.idade}`)
